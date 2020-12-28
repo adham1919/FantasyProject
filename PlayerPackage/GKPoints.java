ab0cf870e45b4a925dcc2eee5b7bd315f0f958e8
marwanayman318 <76461549+marwanayman318@users.noreply.github.com>
2020-12-28 10:20:14 +0200

    sprint 2 update 2

---
+++ PlayerPackage/GKPoints.java
@@ -0,0 +1,20 @@
+package PlayerPackage;
+
+public class GKPoints implements PointCalculator {
+	 public int calPoints(MatchStatus m)
+	{
+			int tot=0;
+			if(m.getTime()>60) tot+=2;
+			else tot+=1;
+			tot+=m.getGoals()*6;
+			tot+=m.getAssists()*3;
+			tot-=(m.getConcededGoals()/2);
+			tot-=m.getOwnGoals()*2;
+			if(m.hadCleanSh()) tot+=4;
+			if(m.hadYCard()) tot-=1;
+			if(m.hadRCard()) tot-=3;
+			if(m.hadPenSaved()) tot+=5;
+			if(m.hadPenMissd()) tot-=2;
+	        return tot;
+	}
+}
